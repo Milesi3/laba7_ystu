92447fbff900861fdb32159f95eec24f2ca7bef0
milesi <irina-rxv@yandex.ru>
2023-05-16 15:08:13 +0300

    Add ctring Good Job

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Have nice day!");
+            Console.WriteLine("Good Job");
         }
     }
 }
